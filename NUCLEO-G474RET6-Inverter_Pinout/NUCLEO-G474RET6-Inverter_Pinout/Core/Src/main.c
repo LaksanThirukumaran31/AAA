dca5032fb6002530b70bc1e015e1437479606f50
Thirukumaran Laksan <laksan461@gmail.com>
2023-10-25 10:03:49 +0200

    command shell

--- NUCLEO-G474RET6-Inverter_Pinout/NUCLEO-G474RET6-Inverter_Pinout/Core/Src/main.c
+++ NUCLEO-G474RET6-Inverter_Pinout/NUCLEO-G474RET6-Inverter_Pinout/Core/Src/main.c
@@ -24,6 +24,7 @@
 #include "usart.h"
 #include "gpio.h"
 
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "mylibs/shell.h"
